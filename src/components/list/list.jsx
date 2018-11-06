64230b19122c0bd17b2880ad1808444938209941
Edgar Ehrenberg <EdgarErenbergs@rambler.ru>
2018-11-06 15:33:42 +0300

    Added Two Components

---
+++ src/components/list/list.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+/* Components */
+import ListItem from "../list-item/list-item";
+
+/* Styles */
+import "./list.scss";
+
+const List = ({ todoData, onDeleted, onToggleImportant, onToggleDone }) => {
+    const elements = todoData.map((item) => {
+
+        const { id, label, important, done } = item;
+
+        return (
+            <li key={id} className="list-group-item">
+                <ListItem label={label} important={important} done={done}
+                onDeleted={() => onDeleted(id) }
+                onToggleImportant={() => onToggleImportant(id)}
+                onToggleDone={() => onToggleDone(id) } />
+            </li>
+        );
+    });
+
+    return (
+        <ul className="list-group todo-list">
+            { elements }
+        </ul>
+    );
+};
+
+export default List;
